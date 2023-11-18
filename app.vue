e9f9ca44aec8d11b31aeff6b3146aa7f547096ce
MyNameTsThad <pvpthadgaming@gmail.com>
2023-11-18 19:58:53 +0700

    update site pfp in embed

--- app.vue
+++ app.vue
@@ -10,7 +10,10 @@ useHead({
   meta: [
     { property: 'og:title', content: "IWant2TryHard's Site" },
     { property: 'og:description', content: 'my place on the internet :3' },
-    { property: 'og:image', content: 'https://thaddev.com/favicon.ico' },
+    { property: 'og:image', content: 'https://thaddev.com/siteimg.png' },
+    { property: 'og:image:width', content: '300' },
+    { property: 'og:image:height', content: '300' },
+    { property: 'og:image:type', content: 'image/png' },
     { property: 'og:url', content: 'https://thaddev.com' },
     { name: 'theme-color', content: '#A25DFA' }
   ]
