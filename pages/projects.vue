98f1ab4849be771e13e031d114c39cdb699cbab0
itscrystalline <pvpthadgaming@gmail.com>
2025-04-04 15:20:02 +0700

    added tags

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,13 +1,16 @@
 <template>
   <MainWindow selectedTab="projects">
     <div v-if="status === 'pending'">Loading...</div>
-    <div v-if="status === 'error'">Error fetching projects. ({{ error }})</div>
+    <div v-else-if="status === 'error'">Error fetching projects. ({{ error }})</div>
     <div v-else class="ProjectsFrame">
       <div v-for="post in posts.projects" class="ProjectCard">
         <NuxtImg v-if="post.cover" v-bind:src="post.cover" v-bind:alt="post.title" />
         <div class="content">
           <h1>{{ post.title }}</h1>
           <p>{{ post.description }}</p>
+          <div class="tags">
+            <div class="tag" v-for="tag in post.tags">{{ tag }}</div>
+          </div>
         </div>
         <div class="buttons">
           <NuxtLink v-bind:to="link.link" v-for="link in post.links" target="_blank">
@@ -27,5 +30,5 @@ import "~/assets/css/projects.sass";
 </script>
 
 <script setup lang="ts">
-const { status, error, data: posts } = useFetch("https://server.iw2tryhard.dev/itscrystalline_projects.json", { lazy: true });
+const { status, error, data: posts } = useFetch("/api/projects", { lazy: true });
 </script>
--- server/api/projects.get.ts
+++ server/api/projects.get.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async () => {
-    const posts = await $fetch(
-        "https://server.iw2tryhard.dev/itscrystalline_projects.json",
-    );
-    return posts;
+  const posts = await $fetch(
+    "https://server.iw2tryhard.dev/itscrystalline_projects.json",
+  );
+  return posts;
 });
