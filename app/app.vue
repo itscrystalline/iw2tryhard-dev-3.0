898a009776bc77618d00a31bbcde40d568df4ec3
itscrystalline <crystal@iw2tryhard.dev>
2026-06-04 18:26:34 +0700

    feat: update colors

--- app/app.vue
+++ app/app.vue
@@ -5,21 +5,21 @@
 </template>
 
 <script setup lang="ts">
-const colorMode = useColorMode();
+useColorMode();
 useHead({
-    title: "crystal's site",
-    meta: [
-        { property: "og:title", content: "crystal's site" },
-        { property: "og:description", content: "my place on the internet :3" },
-        { property: "og:image", content: "https://iw2tryhard.dev/siteimg.png" },
-        { property: "og:image:width", content: "300" },
-        { property: "og:image:height", content: "300" },
-        { property: "og:image:type", content: "image/png" },
-        { property: "og:url", content: "https://iw2tryhard.dev" },
-        { name: "theme-color", content: "#A25DFA" },
-    ],
-    htmlAttrs: {
-        class: "pride",
-    },
+	title: "crystal's site",
+	meta: [
+		{ property: "og:title", content: "crystal's site" },
+		{ property: "og:description", content: "my place on the internet :3" },
+		{ property: "og:image", content: "https://iw2tryhard.dev/siteimg.png" },
+		{ property: "og:image:width", content: "300" },
+		{ property: "og:image:height", content: "300" },
+		{ property: "og:image:type", content: "image/png" },
+		{ property: "og:url", content: "https://iw2tryhard.dev" },
+		{ name: "theme-color", content: "#f5c2e7" },
+	],
+	htmlAttrs: {
+		class: "pride",
+	},
 });
 </script>
